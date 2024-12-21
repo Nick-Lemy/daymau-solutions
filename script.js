143e64f2b9c962ab5c674304f6830aa0d36a7ac3
Nick-Lemy <n.kayiranga@alustudent.com>
2024-12-21 12:13:54 +0200

    Logo added

---
+++ script.js
@@ -0,0 +1,12 @@
+const items = document.querySelectorAll(".item");
+
+items.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    element.classList.toggle("active");
+    items.forEach((item) => {
+      if (item !== element) {
+        item.classList.remove("active");
+      }
+    });
+  });
+});
